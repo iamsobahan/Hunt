411558ed44eb8cd8fae0524eb87c467be9fa5141
Sobahan.webhunter <sobahan.webhunter@gmail.com>
2021-05-05 15:12:12 +0600

    completing hunt

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
 
+//  smooth scrolling  start
+
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100 
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  //  smooth scrooling end
+
+//  back to top start
 $(".backtotop").click(function(){
   $("html").animate({
     scrollTop: 0,
@@ -7,8 +51,10 @@ $(".backtotop").click(function(){
 })
 
 
+//  back to top end
 
 
+//  fixed menu start plus back to top fade in fade out start
 $(window).scroll(function(){
  
   if($(window).scrollTop() > 200){
@@ -26,6 +72,10 @@ $(window).scroll(function(){
     $(".backtotop").fadeOut()
   }
 })
+
+
+//  fixed menu start plus back to top fade in fade out end
+
 
 
 // banner slider start
